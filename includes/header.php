ee9de9ec85f2d4152268918c0df2fadf1392bc74
matheus.sesso <matheus.sesso@DESKTOP-001343>
2021-08-20 16:44:37 -0300

    correção de responsividade

--- includes/header.php
+++ includes/header.php
@@ -31,6 +31,7 @@
         #svg-map a:hover .circle { fill:#8E0F12 !important; cursor:pointer }
 
         @media screen and (max-width: 550px) {#svg-map{ width: 350px; margin-top: -30px; margin-bottom: -30px; padding-right: 10px;}}
+        @media screen and (max-width: 350px) {#svg-map{ width: 300px; margin-top: -30px; margin-bottom: -30px; padding-right: 10px;}}
     </style>
 </head>
 <body>
