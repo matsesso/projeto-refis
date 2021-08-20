2d3e9d5c813351ce718d04ea113b1811673d7650
matheus.sesso <matheus.sesso@DESKTOP-001343>
2021-08-20 16:00:49 -0300

    adição da database.sql

---
+++ database.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 20-Ago-2021 às 20:58
+-- Versão do servidor: 10.4.18-MariaDB
+-- versão do PHP: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+CREATE TABLE `tabela` (
+  `cro` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO `tabela` (`cro`) VALUES
+('AC'),
+('AL'),
+('AM'),
+('AP'),
+('BA'),
+('CE'),
+('DF'),
+('ES'),
+('GO'),
+('MA'),
+('MG'),
+('MS'),
+('MT'),
+('PA'),
+('PB'),
+('PE'),
+('PI'),
+('PR'),
+('RJ'),
+('RN'),
+('RO'),
+('RR'),
+('RS'),
+('SC'),
+('SE'),
+('SP'),
+('TO');
+
+ALTER TABLE `tabela`
+  ADD PRIMARY KEY (`cro`);
+COMMIT;
